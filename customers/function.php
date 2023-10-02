97fe54ea7657e1412522132bf9c242185c670659
rafsanrjkhan <rjkhan104@gmail.com>
2023-10-02 16:10:20 +0600

    function.php

---
+++ customers/function.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once '../inc/dbcon.php';
+function getCustomerList(){
+    global $conn;
+
+    $query = "SELECT * FROM `customers`";
+    $query_run = mysqli_query($conn, $query);
+
+    if($query_run){
+        if(mysqli_num_rows($query_run) > 0){
+            $res = mysqli_fetch_all($query_run, MYSQLI_ASSOC);
+            $data = [
+                'status' => 200,
+                'message' => 'Customer List Fetched Successfully',
+            ];
+            header("HTTP/1.0 200 OK");
+            echo json_encode($data);
+        } else {
+            $data = [
+                'status' => 404,
+                'message' => 'No Customer Found',
+            ];
+            header("HTTP/1.0 404 No Customer Found");
+            echo json_encode($data);
+        }
+
+    } else {
+        $data = [
+            'status' => 500,
+            'message' => 'Internal Server Error',
+        ];
+        header("HTTP/1.0 405 Internal Server Error");
+        echo json_encode($data);
+    }
+}
